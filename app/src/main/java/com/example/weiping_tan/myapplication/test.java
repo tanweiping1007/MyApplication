5f91ae3c5b3b92e3d89f2ee47eeb06d2697b406b
tanweiping1007 <weiping1007@gmail.com>
2016-01-09 15:58:19 +0800

    add

--- app/src/main/java/com/example/weiping_tan/myapplication/test.java
+++ app/src/main/java/com/example/weiping_tan/myapplication/test.java
@@ -5,6 +5,5 @@ import android.app.Activity;
 /**
  * Created by weiping-tan on 9/1/2016.
  */
-public class test2 extends Activity {
-    private String abc;
+public class test extends Activity {
 }
